4aa439e780562d7810dc8d2903081ae891de0dc2
cuarezdani <cuarezstefany@gmail.com>
2023-02-16 12:27:38 -0400

    Falta solo Funcion de Calcular

--- src/data.js
+++ src/data.js
@@ -1,22 +1,37 @@
-// funcionpara filtrar por datos 
-export const filterData=(data, casas) => {
-  switch(casas){
-    case "Gryffindor":
-      const casagryffindor = data.characters.filter(characters => characters.house == "Gryffindor");
-      console.log(casagryffindor)
-      break;
-    case "Ravenclaw":
-      const casaravenclaw = data.characters.filter(characters => characters.house == "Ravenclaw");
-      console.log(casaravenclaw)
-      break;
-    case "Slytherin":
-      const casaslytherin = data.characters.filter(characters => characters.house == "Slytherin");
-      console.log(casaslytherin)
-      break;
-    case "Hufflepuff":
-      const casahufflepuff = data.characters.filter(characters => characters.house == "Hufflepuff");
-      console.log(casahufflepuff)
-      break;
-    }
+// funcion para filtrar por datos 
+export const filterData=(characters, casas) => {
+ 
+  //Filtrando por cada una de las casas (4 casas)
+  const $casas=document.getElementById("root");
+  const filterCasa = characters.filter(character=> character.house === casas) 
+  console.log("Nombre de la casa: " + casas);
+  console.log("Trae toda la data correspondiente a una casa", filterCasa);
+ 
+  //muestra los personajes que hay por cada una de las casas
+  $casas.innerHTML = ""   //está vaciando el contenido HTML dentro del elemento con la ID "casas". Esto se hace para que solo se muestren los elementos filtrados y no se dupliquen o agreguen elementos adicionales a cada vez que se ejecute el código
+  filterCasa.forEach(item => {  //Inicio del bucle, Cada elemento del array se representa con la variable .filterCasaitem
+    const $div = document.createElement ("div") //  crea un nuevo elemento de división en el documento HTML. La variable se utiliza para hacer referencia a este elemento recién creado.$div
+    $div.innerHTML= item.name  // establece el contenido HTML del elemento de división recién creado como el nombre del elemento actual en el array 'filterCasaname del objeto 'itemitem.
+    $casas.appendChild ($div)  // El método 'appendappendChild() agrega el elemento proporcionado como un hijo del elemento actual. En este caso, estamos agregando el elemento de división recién creado (representado por la variable '$$div) como un hijo del elemento con la ID "casas".
+  } )
+ 
 };
+
+//Funcion para Ordenar de A-Z
+export const OrdenarP=(personajes) => {
+  personajes.sort((a, b) => 
+  {
+    if (a.name > b.name){
+      return 1;
+    }
+    if (a.name < b.name){
+      return -1;
+    }
+    return 0;
+  }
+  ); 
+  console.log(personajes);
+  return personajes;
+};
+
 
--- src/main.js
+++ src/main.js
@@ -1,18 +1,45 @@
 // import la funcion de data.js
 import { filterData } from './data.js';
+import {OrdenarP} from './data.js';
 
 //importando la data de harry potter
 import data from './data/harrypotter/data.js';
 
-const casas=document.getElementsByClassName("casas");
-//console.log(casas) // array recorrer
+let casaSeleccionada;
 
-for (let index = 0; index < casas.length; index++) {
-  // console.log(casas[index]) // tenemos cada li
+const MenuCasas = document.getElementsByClassName ("casas")
+// MenuCasas.forEach (element => element.addEventListener ("click", filterData (data, element.id)) ) 
 
-    casas[index].addEventListener("click", function() {
-       // console.log(casas[index].id) // obtenemos id de cada li
-        filterData(data,casas[index].id) //enviamos argumentos 
-    })
+for (let index = 0; index < MenuCasas.length; index++) {
+  //console.log(MenuCasas[index]) // tenemos cada li
+  MenuCasas[index].addEventListener("click", function() {
+    // console.log(casas[index].id) // obtenemos id de cada li
+    casaSeleccionada = MenuCasas[index].id
+    filterData(data.characters,casaSeleccionada) //enviamos argumentos 
+  })
 }
+
+//ordena la data de personajes dependiendo de la casa
+const OrdenPersanajes = document.getElementsByClassName ("Ordenes")
+for (let index = 0; index < OrdenPersanajes.length; index++) {
+  OrdenPersanajes[index].addEventListener("click", function() {
+    const dataordenada= OrdenarP(data.characters,OrdenPersanajes[index].id)
+    filterData(dataordenada,casaSeleccionada)
+  })
+}
+
+
+//Funcion para Contar
+const characters = data.characters
+const count= {}
+
+characters.forEach ((character) =>{
+  if(!count[character.species]) {
+    count[character.species] = 1
+  } else {count[character.species] += 1}
+
+} )
+console.log(count);
+
+
 
